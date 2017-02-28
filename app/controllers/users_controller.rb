125a5401f8ab4ba017b70e77f1dcaaeb2d9881ea
Alexander Diaz <alexitofrancis@gmail.com>
2017-02-28 08:01:28 +0000

    Added users index action and view. Well and profile image errors still exist.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
-  # GET to users/:id
+  # GET to /users/
+  def index
+    
+  end
+  
+  # GET to /users/:id
   def show
     @user = User.find(params[:id])
   end
